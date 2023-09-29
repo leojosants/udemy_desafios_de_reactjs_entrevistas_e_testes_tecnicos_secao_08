cce5c3b83f5e2eb86e4ae866a7a3b044721c70e1
Leonardo Santos <leonardojosantos@gmail.com>
2023-09-28 22:05:54 -0300

    class 93 - exercise 36: to do list with context api solution

--- gerenciamento_de_estado/src/App.jsx
+++ gerenciamento_de_estado/src/App.jsx
@@ -1,17 +1,35 @@
 import './App.css'
 import Counter from './components/Counter';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
+import { TaskProvider } from './context/TaskContext';
 import store from './stor';
 import { Provider } from 'react-redux';
+
+const style_div_externa = [{ border: '1px solid red', marginBottom: '20px', padding: '1rem' }];
 
 function App() {
   return (
     <>
       {/* 1 - counter with redux */}
-      <div style={{ border: '1px solid red', marginBottom: '20px', padding: '1rem' }}>
+      <div style={style_div_externa[0]}>
+       
         <h2>1 - Contador com Redux</h2>
+
         <Provider store={store}>
           <Counter />
         </Provider>
+      </div>
+
+      {/* 2 - to-do with context api */}
+      <div style={style_div_externa[0]}>
+       
+        <h2>2 - Todo com context API</h2>
+
+        <TaskProvider>
+          <TaskForm />
+          <TaskList />
+        </TaskProvider>
       </div>
     </>
   );
